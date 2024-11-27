ed7c580ffe8be1b76c767d5322fd5b7defc7df94
JerichoCCO <133850572+JerichoCCO@users.noreply.github.com>
2024-11-28 06:32:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-import streamlit as st 
+import streamlit as st
 
-st.write("Hello World")
+# Set page configuration without an icon
+st.set_page_config(page_title="Vehicle Classification", layout="wide")
+
+# Page title
+st.write(
+    "<div style='text-align: center; font-size: 50px;'>Welcome to Vehicle Classification</div>",
+    unsafe_allow_html=True,
+)
