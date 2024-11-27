1fde61abc7eed762514ede523c05888771253028
JerichoCCO <133850572+JerichoCCO@users.noreply.github.com>
2024-11-28 06:35:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # Set page configuration without an icon
-st.set_page_config(page_title="Plantar Classification System", layout="wide")
+st.set_page_config(page_title="Plantar Pressure Distribution Classification System", layout="wide")
 
 # Page title
 st.write(
-    "<div style='text-align: center; font-size: 50px;'>Welcome to Vehicle Classification</div>",
+    "<div style='text-align: center; font-size: 50px;'>Welcome to Plantar Pressure Distribution Classification System</div>",
     unsafe_allow_html=True,
 )
